da374c6b66d212d7f9b538878b203a706d6cb276
Abdul Rahman <abdulr6503@gmail.com>
2025-06-30 03:10:59 +0530

    fixed bugs

--- src/context/client-wallet-provider.tsx
+++ src/context/client-wallet-provider.tsx
@@ -11,6 +11,8 @@ import {
 } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
+import '@solana/wallet-adapter-react-ui/styles.css';
+
 
 export function ClientWalletProvider({
   children,
